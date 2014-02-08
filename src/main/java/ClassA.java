41a4df53f9923ecba9704ac6402ba8a8acddd344
John Sigvald Skauge <thejohnskauge@gmail.com>
2014-02-08 18:29:44 +0100

    Added some functionallity to classes

--- src/main/java/ClassA.java
+++ src/main/java/ClassA.java
@@ -2,4 +2,12 @@
  * Created by modulus on 07/02/14.
  */
 public class ClassA {
+
+    public void a() throws Exception {
+        throw new Exception("a method");
+    }
+
+    public String getA(){
+        return "aMethod";
+    }
 }
--- src/main/java/ClassB.java
+++ src/main/java/ClassB.java
@@ -2,4 +2,12 @@
  * Created by modulus on 07/02/14.
  */
 public class ClassB {
+
+    public void b(){
+        throw new RuntimeException("b");
+    }
+
+    public String getB(){
+        return "bMethod";
+    }
 }
